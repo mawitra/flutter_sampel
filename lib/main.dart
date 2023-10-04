548ef807a81af60ea3eb650ea9b31a4a6b2fc1a6
mawitra <madadwisaputra551@gmail.com>
2023-10-04 02:51:48 -0700

    flutter_ui_login

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:testing/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (context, child) {
+        return MaterialApp(
+          home: LoginPage(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
+  }
+}
